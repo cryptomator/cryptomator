282fd5eceed2df3d765efb10acfd2301f6eb0600
Sebastian Stenzel <sebastianstenzel@gmail.com>
2015-12-29 22:09:12 +0100

    added copyright header [ci skip]

--- main/filesystem-inmemory/src/main/java/org/cryptomator/filesystem/inmem/InMemoryReadableFile.java
+++ main/filesystem-inmemory/src/main/java/org/cryptomator/filesystem/inmem/InMemoryReadableFile.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Sebastian Stenzel and others.
+ * This file is licensed under the terms of the MIT license.
+ * See the LICENSE.txt file for more info.
+ *
+ * Contributors:
+ *     Sebastian Stenzel - initial API and implementation
+ *******************************************************************************/
 package org.cryptomator.filesystem.inmem;
 
 import java.io.UncheckedIOException;
