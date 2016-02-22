61f1afba87d7ff0ecdf4cf31fcb0d793595a1016
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2016-02-22 23:48:01 +0100

    deleted legacy code

--- main/crypto-api/src/main/java/org/cryptomator/crypto/exceptions/WrongPasswordException.java
+++
@@ -1,9 +0,0 @@
-package org.cryptomator.crypto.exceptions;
-
-public class WrongPasswordException extends MasterkeyDecryptionException {
-	private static final long serialVersionUID = -602047799678568780L;
-
-	public WrongPasswordException() {
-		super("Wrong password.");
-	}
-}
