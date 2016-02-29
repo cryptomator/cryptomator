9abc0aab8340dfe04a7e8cbf25adabaf161b5f17
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2016-02-29 22:27:52 +0100

    updated copyrights

--- main/ui/src/main/java/org/cryptomator/ui/util/ListenerRegistry.java
+++ main/ui/src/main/java/org/cryptomator/ui/util/ListenerRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 cryptomator.org
+ * Copyright (c) 2014, 2016 cryptomator.org
  * This file is licensed under the terms of the MIT license.
  * See the LICENSE.txt file for more info.
  * 
