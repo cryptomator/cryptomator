12b56e0a5531c4902308a75f8f87c15fa1e09947
jellemdekker <jellemdekker@users.noreply.github.com>
2018-12-23 13:00:36 +0100

    Implemented a custom TextFieldSkin for the auto-filled SecPasswordField used for unlocking a vault with a password from the system's keychain. This skin makes it appear as if the auto-filled password is always 50 characters long. Additionally, the skin disposes of itself upon a key press on the associated SecPasswordField.

---
+++ main/ui/src/main/java/org/cryptomator/ui/controls/AutofilledSecPasswordFieldSkin.java
@@ -0,0 +1,50 @@
+package org.cryptomator.ui.controls;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.skin.TextFieldSkin;
+import javafx.scene.input.KeyEvent;
+
+public class AutofilledSecPasswordFieldSkin extends TextFieldSkin {
+
+    /**
+     * Defines the length of the masked password to be displayed in
+     * place of the real password.
+     */
+    private static final int AUTOFILLED_PASSWORD_MASK_LENGTH = 50;
+    /**
+     * Identical to {@link TextFieldSkin#BULLET TextFieldSkin.BULLET}.
+     */
+    private static final char BULLET = '\u25cf';
+
+    /**
+     * Creates a new AutofilledSecPasswordFieldSkin instance, that makes it appear
+     * as if the password field contains a password with the length of
+     * {@link AutofilledSecPasswordFieldSkin#AUTOFILLED_PASSWORD_MASK_LENGTH}. On
+     * a key press, the password field is swiped and this skin is removed, so that the
+     * auto-filled password's length is never disclosed and a newly entered password's
+     * length is properly displayed.
+     *
+     * @param passwordField The password field that this skin should be installed onto.
+     */
+    public AutofilledSecPasswordFieldSkin(SecPasswordField passwordField) {
+        super(passwordField);
+        passwordField.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<>() {
+            @Override
+            public void handle(KeyEvent event) {
+                passwordField.swipe();
+                passwordField.removeEventHandler(KeyEvent.KEY_PRESSED, this);
+                dispose();
+            }
+        });
+    }
+
+    @Override
+    protected String maskText(String txt) {
+        int n = AUTOFILLED_PASSWORD_MASK_LENGTH;
+        StringBuilder passwordBuilder = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            passwordBuilder.append(BULLET);
+        }
+        return passwordBuilder.toString();
+    }
+}
