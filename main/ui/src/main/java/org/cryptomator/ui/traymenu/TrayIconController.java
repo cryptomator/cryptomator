2024c95be5decfbea0419309a04649af60bdb6aa
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2019-08-01 16:02:09 +0200

    add actionlistener to tray icon

--- main/ui/src/main/java/org/cryptomator/ui/traymenu/TrayIconController.java
+++ main/ui/src/main/java/org/cryptomator/ui/traymenu/TrayIconController.java
@@ -1,6 +1,7 @@
 package org.cryptomator.ui.traymenu;
 
 import javafx.beans.Observable;
+import org.apache.commons.lang3.SystemUtils;
 import org.cryptomator.common.settings.Settings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +10,13 @@ import javax.inject.Inject;
 import java.awt.AWTException;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 @TrayMenuScoped
 public class TrayIconController {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(TrayIconController.class);
 
 	private final Settings settings;
@@ -27,9 +31,14 @@ public class TrayIconController {
 		this.imageFactory = imageFactory;
 		this.trayIcon = new TrayIcon(imageFactory.loadImage(), "Cryptomator", trayMenuController.getMenu());
 	}
-	
+
 	public void initializeTrayIcon() {
 		settings.theme().addListener(this::themeChanged);
+
+		if (SystemUtils.IS_OS_WINDOWS) {
+			// TODO: test on windows: is this a double click?
+			trayIcon.addActionListener(trayMenuController::showMainWindow);
+		}
 
 		try {
 			SystemTray.getSystemTray().add(trayIcon);
@@ -37,13 +46,12 @@ public class TrayIconController {
 		} catch (AWTException e) {
 			LOG.error("Error adding tray icon", e);
 		}
-		
+
 		trayMenuController.initTrayMenu();
 	}
 
 	private void themeChanged(@SuppressWarnings("unused") Observable observable) {
 		trayIcon.setImage(imageFactory.loadImage());
 	}
-
 
 }
