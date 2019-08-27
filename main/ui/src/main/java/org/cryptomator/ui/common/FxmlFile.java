71e414ae5c330aecadb667ebc41a6364ab999721
Armin Schrenk <armin.schrenk@zoho.eu>
2019-08-27 15:23:48 +0200

    formatting

--- main/ui/src/main/java/org/cryptomator/ui/common/FxmlFile.java
+++ main/ui/src/main/java/org/cryptomator/ui/common/FxmlFile.java
@@ -6,13 +6,13 @@ public enum FxmlFile {
 	ADDVAULT_NEW_NAME("/fxml/addvault_new_name.fxml"), //
 	ADDVAULT_NEW_LOCATION("/fxml/addvault_new_location.fxml"), //
 	ADDVAULT_NEW_PASSWORD("/fxml/addvault_new_password.fxml"), //
-	CHANGEPASSWORD("/fxml/changepassword.fxml"),
+	CHANGEPASSWORD("/fxml/changepassword.fxml"), //
 	MAIN_WINDOW("/fxml/main_window.fxml"), //
 	PREFERENCES("/fxml/preferences.fxml"), //
-	QUIT("/fxml/quit.fxml"),
+	QUIT("/fxml/quit.fxml"), //
 	REMOVE_VAULT("/fxml/remove_vault.fxml"), //
 	UNLOCK("/fxml/unlock2.fxml"), // TODO rename
-	UNLOCK_SUCCESS("/fxml/unlock_success.fxml"),
+	UNLOCK_SUCCESS("/fxml/unlock_success.fxml"), //
 	VAULT_OPTIONS("/fxml/vault_options.fxml");
 
 	private final String filename;
