a2f3f5d254795a7c21f8ec487c7a18ca33455397
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2019-12-18 14:58:36 +0100

    fixes #1018, references #979

--- main/ui/src/test/java/org/cryptomator/ui/common/PasswordStrengthUtilTest.java
+++ main/ui/src/test/java/org/cryptomator/ui/common/PasswordStrengthUtilTest.java
@@ -1,6 +1,7 @@
 package org.cryptomator.ui.common;
 
 import com.google.common.base.Strings;
+import org.cryptomator.common.Environment;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ public class PasswordStrengthUtilTest {
 
 	@Test
 	public void testLongPasswords() {
-		PasswordStrengthUtil util = new PasswordStrengthUtil(Mockito.mock(ResourceBundle.class));
+		PasswordStrengthUtil util = new PasswordStrengthUtil(Mockito.mock(ResourceBundle.class), Mockito.mock(Environment.class));
 		String longPw = Strings.repeat("x", 10_000);
 		Assertions.assertTimeout(Duration.ofSeconds(5), () -> {
 			util.computeRate(longPw);
@@ -21,9 +22,9 @@ public class PasswordStrengthUtilTest {
 	}
 	
 	@Test
-	@Disabled("waiting on upstream fix")
+	@Disabled("waiting on upstream fix") // https://github.com/nulab/zxcvbn4j/issues/54
 	public void testIssue979() {
-		PasswordStrengthUtil util = new PasswordStrengthUtil(Mockito.mock(ResourceBundle.class));
+		PasswordStrengthUtil util = new PasswordStrengthUtil(Mockito.mock(ResourceBundle.class), Mockito.mock(Environment.class));
 		int result1 = util.computeRate("backed derrick buckling mountains glove client procedures desire destination sword hidden ram");
 		int result2 = util.computeRate("backed derrick buckling mountains glove client procedures desire destination sword hidden ram escalation");
 		Assertions.assertEquals(4, result1);
