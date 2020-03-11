99ca7168e367b10d22b539d96ff03e52439f22b4
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-11 17:00:02 +0100

    reworking the wrong file alert dialogue

--- main/ui/src/main/java/org/cryptomator/ui/wrongfilealert/WrongFileAlertController.java
+++ main/ui/src/main/java/org/cryptomator/ui/wrongfilealert/WrongFileAlertController.java
@@ -1,5 +1,8 @@
 package org.cryptomator.ui.wrongfilealert;
 
+import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import org.cryptomator.ui.common.FxController;
 
@@ -8,14 +11,23 @@ import javax.inject.Inject;
 @WrongFileAlertScoped
 public class WrongFileAlertController implements FxController {
 
+	private static final String DOCUMENTATION_URI = "https://docs.cryptomator.org";
+
+	private Application app;
 	private final Stage window;
 
 	@Inject
-	public WrongFileAlertController(@WrongFileAlertWindow Stage window) {
+	public WrongFileAlertController(@WrongFileAlertWindow Stage window, Application app) {
 		this.window = window;
+		this.app = app;
 	}
 
 	public void close() {
 		window.close();
+	}
+
+	@FXML
+	public void openDocumentation() {
+		app.getHostServices().showDocument(DOCUMENTATION_URI);
 	}
 }
