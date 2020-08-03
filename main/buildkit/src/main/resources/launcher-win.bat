51f7d74e4a786bbbe035c2de0e047a37d53b162c
JaniruTEC <JaniruTEC@gmail.com>
2020-08-03 21:22:22 +0200

    Changed cryptomator.mountPointsDir-property in Windows Configs
    Changed cryptomator.mountPointsDir-property in Windows Configs from "~/Mount" to "~/Cryptomator

--- main/buildkit/src/main/resources/launcher-win.bat
+++ main/buildkit/src/main/resources/launcher-win.bat
@@ -4,7 +4,7 @@ java ^
 	-Dcryptomator.settingsPath="~/AppData/Roaming/Cryptomator/settings.json" ^
 	-Dcryptomator.ipcPortPath="~/AppData/Roaming/Cryptomator/ipcPort.bin" ^
 	-Dcryptomator.logDir="~/AppData/Roaming/Cryptomator" ^
-	-Dcryptomator.mountPointsDir="~/Mount" ^
+	-Dcryptomator.mountPointsDir="~/Cryptomator" ^
 	-Dcryptomator.keychainPath="~/AppData/Roaming/Cryptomator/keychain.json" ^
 	-Xss20m ^
 	-Xmx512m ^
