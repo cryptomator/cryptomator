51aac156225b290f4c2b677c9ae55e23ba77ab37
Armin Schrenk <armin.schrenk@skymatic.de>
2020-11-20 12:23:44 +0100

    Changed forced lock description and clean up

--- main/ui/src/main/java/org/cryptomator/ui/lock/LockForcedController.java
+++ main/ui/src/main/java/org/cryptomator/ui/lock/LockForcedController.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -28,18 +28,20 @@ public class LockForcedController implements FxController {
 		this.window.setOnHiding(this::windowClosed);
 	}
 
+	@FXML
 	public void cancel() {
 		forceLockDecisionLock.interacted(LockModule.ForceLockDecision.CANCEL);
 		window.close();
 	}
 
-	public void confirmForcedLock(ActionEvent actionEvent) {
+	@FXML
+	public void confirmForcedLock() {
 		forceLockDecisionLock.interacted(LockModule.ForceLockDecision.FORCE);
 		window.close();
 	}
 
 	private void windowClosed(WindowEvent windowEvent) {
-		// if not already interacted, mark this workflow as cancelled:
+		// if not already interacted, set the decision to CANCEL
 		if (forceLockDecisionLock.awaitingInteraction().get()) {
 			LOG.debug("Lock canceled in force-lock-phase by user.");
 			forceLockDecisionLock.interacted(LockModule.ForceLockDecision.CANCEL);
