3b73544766985a1e7b9fbe0c1ecdc5275cb5d741
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2020-12-08 12:56:44 +0100

    Simplified observation of visible stages

--- main/ui/src/main/java/org/cryptomator/ui/fxapp/FxApplicationModule.java
+++ main/ui/src/main/java/org/cryptomator/ui/fxapp/FxApplicationModule.java
@@ -18,7 +18,6 @@ import org.cryptomator.ui.unlock.UnlockComponent;
 
 import javax.inject.Named;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableSet;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -30,12 +29,6 @@ import java.util.List;
 
 @Module(includes = {UpdateCheckerModule.class}, subcomponents = {MainWindowComponent.class, PreferencesComponent.class, UnlockComponent.class, QuitComponent.class, ErrorComponent.class})
 abstract class FxApplicationModule {
-
-	@Provides
-	@FxApplicationScoped
-	static ObservableSet<Stage> provideVisibleStages() {
-		return FXCollections.observableSet();
-	}
 
 	@Provides
 	@Named("windowIcons")
@@ -56,16 +49,9 @@ abstract class FxApplicationModule {
 
 	@Provides
 	@FxApplicationScoped
-	static StageFactory provideStageFactory(@Named("windowIcons") List<Image> windowIcons, ObservableSet<Stage> visibleStages) {
+	static StageFactory provideStageFactory(@Named("windowIcons") List<Image> windowIcons) {
 		return new StageFactory(stage -> {
 			stage.getIcons().addAll(windowIcons);
-			stage.showingProperty().addListener((observableValue, wasShowing, isShowing) -> {
-				if (isShowing) {
-					visibleStages.add(stage);
-				} else {
-					visibleStages.remove(stage);
-				}
-			});
 		});
 	}
 
