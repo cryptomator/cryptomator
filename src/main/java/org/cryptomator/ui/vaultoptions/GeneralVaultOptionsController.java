8a494c13e6398d8c8d9de1c0ea093da5638c1075
Armin Schrenk <armin.schrenk@skymatic.de>
2021-06-08 14:47:22 +0200

    clean up

--- src/main/java/org/cryptomator/ui/vaultoptions/GeneralVaultOptionsController.java
+++ src/main/java/org/cryptomator/ui/vaultoptions/GeneralVaultOptionsController.java
@@ -1,6 +1,7 @@
 package org.cryptomator.ui.vaultoptions;
 
 import org.cryptomator.common.Constants;
+import org.cryptomator.common.settings.VaultSettings;
 import org.cryptomator.common.settings.WhenUnlocked;
 import org.cryptomator.common.vaults.Vault;
 import org.cryptomator.ui.common.FxController;
@@ -25,6 +26,7 @@ public class GeneralVaultOptionsController implements FxController {
 
 	private final Stage window;
 	private final Vault vault;
+	private final VaultSettings vaultSettings;
 	private final HealthCheckComponent.Builder healthCheckWindow;
 	private final ResourceBundle resourceBundle;
 
@@ -38,23 +40,24 @@ public class GeneralVaultOptionsController implements FxController {
 	GeneralVaultOptionsController(@VaultOptionsWindow Stage window, @VaultOptionsWindow Vault vault, HealthCheckComponent.Builder healthCheckWindow, ResourceBundle resourceBundle) {
 		this.window = window;
 		this.vault = vault;
+		this.vaultSettings = vault.getVaultSettings();
 		this.healthCheckWindow = healthCheckWindow;
 		this.resourceBundle = resourceBundle;
 	}
 
 	@FXML
 	public void initialize() {
-		vaultName.textProperty().set(vault.getVaultSettings().displayName().get());
+		vaultName.textProperty().set(vaultSettings.displayName().get());
 		vaultName.focusedProperty().addListener(this::trimAndSetVaultNameOnFocusLoss);
 		vaultName.setTextFormatter(new TextFormatter<>(this::removeWhitespaces));
 
-		unlockOnStartupCheckbox.selectedProperty().bindBidirectional(vault.getVaultSettings().unlockAfterStartup());
+		unlockOnStartupCheckbox.selectedProperty().bindBidirectional(vaultSettings.unlockAfterStartup());
 
 		actionAfterUnlockChoiceBox.getItems().addAll(WhenUnlocked.values());
-		actionAfterUnlockChoiceBox.valueProperty().bindBidirectional(vault.getVaultSettings().actionAfterUnlock());
+		actionAfterUnlockChoiceBox.valueProperty().bindBidirectional(vaultSettings.actionAfterUnlock());
 		actionAfterUnlockChoiceBox.setConverter(new WhenUnlockedConverter(resourceBundle));
 
-		int maxClearTextFileNameLength = vault.getVaultSettings().maxCleartextFilenameLength().get();
+		int maxClearTextFileNameLength = vaultSettings.maxCleartextFilenameLength().get();
 		filenameLengthRestrictionCheckbox.selectedProperty().set(maxClearTextFileNameLength != Constants.UNKNOWN_CLEARTEXT_FILENAME_LENGTH_LIMIT && maxClearTextFileNameLength != Constants.UNLIMITED_CLEARTEXT_FILENAME_LENGTH);
 		filenameLengthRestrictionCheckbox.selectedProperty().addListener(this::resetFileNameLimit);
 
@@ -64,21 +67,21 @@ public class GeneralVaultOptionsController implements FxController {
 
 	private void resetFileNameLimit(Observable observable, Boolean wasTicked, Boolean isTicked) {
 		if (!isTicked) {
-			vault.getVaultSettings().maxCleartextFilenameLength().set(Constants.UNKNOWN_CLEARTEXT_FILENAME_LENGTH_LIMIT);
+			vaultSettings.maxCleartextFilenameLength().set(Constants.UNKNOWN_CLEARTEXT_FILENAME_LENGTH_LIMIT);
 			filenameLengthField.setText("");
 		}
 	}
 
 	private void setFileNameLengthLimitOnFocusLoss(Observable observable, Boolean wasFocused, Boolean isFocused) {
 		if (!isFocused) {
-			vault.getVaultSettings().maxCleartextFilenameLength().set(filenameLengthField.getAsInt().orElse(Constants.UNLIMITED_CLEARTEXT_FILENAME_LENGTH));
+			vaultSettings.maxCleartextFilenameLength().set(filenameLengthField.getAsInt().orElse(Constants.UNLIMITED_CLEARTEXT_FILENAME_LENGTH));
 		}
 	}
 
 	private void trimAndSetVaultNameOnFocusLoss(Observable observable, Boolean wasFocused, Boolean isFocused) {
 		if (!isFocused) {
 			var trimmed = vaultName.getText().trim();
-			vault.getVaultSettings().displayName().set(trimmed);
+			vaultSettings.displayName().set(trimmed);
 		}
 	}
 
