2dac9f565fd92421eba2cb173d45264487930a25
Armin Schrenk <armin.schrenk@skymatic.de>
2021-07-26 17:16:26 +0200

    add default constructor instead of setting a value implicitly

--- src/main/java/org/cryptomator/ui/controls/FontAwesome5Spinner.java
+++ src/main/java/org/cryptomator/ui/controls/FontAwesome5Spinner.java
@@ -5,22 +5,31 @@ import org.cryptomator.ui.common.AutoAnimator;
 
 import javafx.beans.NamedArg;
 import javafx.beans.value.ObservableValue;
+import java.util.Optional;
 
 /**
  * An animated progress spinner using the {@link FontAwesome5IconView} with the spinner glyph.
- *
- * By default, the animation is always played if the icon is visible. To animate on other conditions, use the "spinning" property.
+ * <p>
+ * Using the default constructor, the animation is always played if the icon is visible. To animate on other conditions, use the constructor with the "spinning" property.
  */
-public class FontAwesome5Spinner extends FontAwesome5IconView{
+public class FontAwesome5Spinner extends FontAwesome5IconView {
 
-	private final AutoAnimator animator;
+	private AutoAnimator animator;
+
+	public FontAwesome5Spinner() {
+		new FontAwesome5Spinner(Optional.empty());
+	}
 
 	public FontAwesome5Spinner(@NamedArg("spinning") ObservableValue<Boolean> spinning) {
+		new FontAwesome5Spinner(Optional.of(spinning));
+	}
+
+	private FontAwesome5Spinner(Optional<ObservableValue<Boolean>> animateCondition) {
 		setGlyph(FontAwesome5Icon.SPINNER);
 		var animation = Animations.createDiscrete360Rotation(this);
-		this.animator = AutoAnimator.animate(animation)
-				.afterStop(() -> setRotate(0))
-				.onCondition(spinning == null? visibleProperty():spinning)
+		this.animator = AutoAnimator.animate(animation) //
+				.afterStop(() -> setRotate(0)) //
+				.onCondition(animateCondition.orElse(visibleProperty())) //
 				.build();
 	}
 
