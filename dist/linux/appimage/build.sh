974fc0fa6dac6389389108ff883366c5a4a3c428
what_next <what_next@tuta.io>
2021-10-22 14:23:49 -0600

    Revert "fixing app image creation"
    This reverts commit b9d91fa49f338010c675c62e4aa8a1c2a20a2cfa.

--- dist/linux/appimage/build.sh
+++ dist/linux/appimage/build.sh
@@ -68,10 +68,9 @@ rm -r jni/x86_64-Linux
 # load AppImageTool
 curl -L https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage -o /tmp/appimagetool.AppImage
 chmod +x /tmp/appimagetool.AppImage
-/tmp/appimagetool.AppImage --appimage-extract
 
 # create AppImage
-./squashfs-root/AppRun \
+/tmp/appimagetool.AppImage \
     Cryptomator.AppDir \
     cryptomator-SNAPSHOT-x86_64.AppImage \
     -u 'gh-releases-zsync|cryptomator|cryptomator|latest|cryptomator-*-x86_64.AppImage.zsync'
