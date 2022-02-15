f19587cba763aae2990c436e2c3c458fd0e19fef
Armin Schrenk <armin.schrenk@skymatic.de>
2022-02-15 12:53:01 +0100

    use installer artifact produced by build.ps1 in bundle

--- dist/win/bundle.ps1
+++ dist/win/bundle.ps1
@@ -1,6 +1,12 @@
+
+## Dedup with build.ps1
+$buildDir = Split-Path -Parent $PSCommandPath
+$version = $(mvn -f $buildDir/../../pom.xml help:evaluate -Dexpression="project.version" -q -DforceStdout)
+$semVerNo = $version -replace '(\d\.\d\.\d).*','$1'
+
 ### Creates the Wix Bootstrapper application
 
-& "$env:WIX\bin\candle.exe" bundlewithWinfsp.wxs -ext WixBalExtension -out tmp\
+& "$env:WIX\bin\candle.exe" bundlewithWinfsp.wxs -ext WixBalExtension -out tmp\ -dAppVersion="$semVerNo"
 & "$env:WIX\bin\light.exe" -b . .\tmp\BundlewithWinfsp.wixobj -ext WixBalExtension -out installer\CryptomatorBundle.exe
 
 #Download WinFSP on Demand?
