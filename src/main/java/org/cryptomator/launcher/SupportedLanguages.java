d9aa6ae91a3c7fa81cb618af8d8e260579fdeb8d
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-04-02 12:16:57 +0200

    display "language" chooser in general preferences

---
+++ src/main/java/org/cryptomator/launcher/SupportedLanguages.java
@@ -0,0 +1,12 @@
+package org.cryptomator.launcher;
+
+import java.util.List;
+
+public class SupportedLanguages {
+
+	// these are BCP 47 language codes, not ISO. Note the "-" instead of the "_":
+	public static List<String> LANGUAGAE_TAGS = List.of("ar", "bn", "bs", "ca", "cs", "de", "el", "es", "fil", "fr", "gl", "he", //
+			"hi", "hr", "hu", "id", "it", "ja", "ko", "lv", "mk", "nb", "nl", "nn", "no", "pa", "pl", "pt", "pt-BR", "ro", "ru", //
+			"sk", "sr", "sr-Latn", "sv", "ta", "te", "th", "tr", "uk", "zh", "zh-HK", "zh-TW");
+
+}
