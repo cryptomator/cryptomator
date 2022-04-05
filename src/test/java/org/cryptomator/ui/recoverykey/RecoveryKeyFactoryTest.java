78a50548abc5ca89fbec2631c0d69ec0aa8db5ea
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-04-05 09:19:58 +0200

    optionally allow extended key validation in `RecoveryKeyFactory#validateRecoveryKey`

--- src/test/java/org/cryptomator/ui/recoverykey/RecoveryKeyFactoryTest.java
+++ src/test/java/org/cryptomator/ui/recoverykey/RecoveryKeyFactoryTest.java
@@ -7,10 +7,13 @@ import org.cryptomator.cryptolib.common.MasterkeyFileAccess;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.function.Predicate;
 
 public class RecoveryKeyFactoryTest {
 
@@ -73,6 +76,21 @@ public class RecoveryKeyFactoryTest {
 				investor boot depth left theory snow whereby terminal weekly reject happiness circuit partial cup ad \
 				""");
 		Assertions.assertTrue(result);
+	}
+
+	@ParameterizedTest(name = "success = {0}")
+	@DisplayName("validateRecoveryKey() with extended validation")
+	@ValueSource(booleans = {true, false})
+	public void testValidateValidateRecoveryKeyWithValidKey(boolean extendedValidationResult) {
+		Predicate<byte[]> validator = Mockito.mock(Predicate.class);
+		Mockito.doReturn(extendedValidationResult).when(validator).test(Mockito.any());
+		boolean result = inTest.validateRecoveryKey("""
+				pathway lift abuse plenty export texture gentleman landscape beyond ceiling around leaf cafe charity \
+				border breakdown victory surely computer cat linger restrict infer crowd live computer true written amazed \
+				investor boot depth left theory snow whereby terminal weekly reject happiness circuit partial cup ad \
+				""", validator);
+		Mockito.verify(validator).test(Mockito.any());
+		Assertions.assertEquals(extendedValidationResult, result);
 	}
 
 }
