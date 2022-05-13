87202453b181953ccbbd72b0bc233d9f6de312ce
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-05-13 15:43:43 +0200

    added Swahili to language picker
    [ci skip]

--- src/main/java/org/cryptomator/launcher/SupportedLanguages.java
+++ src/main/java/org/cryptomator/launcher/SupportedLanguages.java
@@ -17,7 +17,7 @@ public class SupportedLanguages {
 	// these are BCP 47 language codes, not ISO. Note the "-" instead of the "_":
 	public static final List<String> LANGUAGAE_TAGS = List.of("en", "ar", "bn", "bs", "ca", "cs", "de", "el", "es", "fil", "fr", "gl", "he", //
 			"hi", "hr", "hu", "id", "it", "ja", "ko", "lv", "mk", "nb", "nl", "nn", "no", "pa", "pl", "pt", "pt-BR", "ro", "ru", "sk", "sr", //
-			"sr-Latn", "sv", "ta", "te", "th", "tr", "uk", "zh", "zh-HK", "zh-TW");
+			"sr-Latn", "sv", "sw", "ta", "te", "th", "tr", "uk", "zh", "zh-HK", "zh-TW");
 
 	@Nullable
 	private final String preferredLanguage;
