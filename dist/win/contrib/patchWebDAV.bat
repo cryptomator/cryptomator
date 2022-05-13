90f523bee820a0d2b5d5e31db64163df30969c3e
Sebastian Stenzel <overheadhunter@users.noreply.github.com>
2022-05-13 15:11:41 +0200

    parameterize patchWebDAV.ps1 and add default values to .bat file (#2251)
    Co-authored-by: Armin Schrenk <armin.schrenk@skymatic.de>

--- dist/win/contrib/patchWebDAV.bat
+++ dist/win/contrib/patchWebDAV.bat
@@ -1,3 +1,7 @@
 @echo off
+:: Default values for Cryptomator builds
+SET LOOPBACK_ALIAS="cryptomator-vault"
+
 cd %~dp0
-powershell -NoLogo -NonInteractive -ExecutionPolicy Unrestricted -Command .\patchWebDAV.ps1
+powershell -NoLogo -NonInteractive -ExecutionPolicy Unrestricted -Command .\patchWebDAV.ps1^
+ -LoopbackAlias %LOOPBACK_ALIAS%
