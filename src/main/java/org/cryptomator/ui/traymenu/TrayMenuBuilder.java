0021f2d37cf3b5f96350e684a2025e80d9d88944
Edward Chow <ledwardchow@gmail.com>
2022-09-16 01:38:42 +1000

    Use 2x icon on Mac menu bar (#2345) (#2346)
    * Use 2x icon on Mac menu bar
    
    Use 2x icon on Mac menu bar
    
    * Fix #2357
    
    Changed Windows tray icon to 2x

--- src/main/java/org/cryptomator/ui/traymenu/TrayMenuBuilder.java
+++ src/main/java/org/cryptomator/ui/traymenu/TrayMenuBuilder.java
@@ -31,8 +31,8 @@ import java.util.ResourceBundle;
 public class TrayMenuBuilder {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TrayMenuBuilder.class);
-	private static final String TRAY_ICON_MAC = "/img/tray_icon_mac.png";
-	private static final String TRAY_ICON = "/img/tray_icon.png";
+	private static final String TRAY_ICON_MAC = "/img/tray_icon_mac@2x.png";
+	private static final String TRAY_ICON = "/img/window_icon_32.png";
 
 	private final ResourceBundle resourceBundle;
 	private final VaultService vaultService;
