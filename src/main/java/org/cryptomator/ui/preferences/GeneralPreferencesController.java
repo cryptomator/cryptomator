30de04e291e08593396ebc786fc59b70ad622f21
Ralph Plawetzki <ralph@purejava.org>
2022-09-20 19:27:44 +0200

    Move CheckBox (injected from JavaFX) below the final fields (injected by Dagger)

--- src/main/java/org/cryptomator/ui/preferences/GeneralPreferencesController.java
+++ src/main/java/org/cryptomator/ui/preferences/GeneralPreferencesController.java
@@ -33,8 +33,8 @@ public class GeneralPreferencesController implements FxController {
 	private final Application application;
 	private final Environment environment;
 	private final List<KeychainAccessProvider> keychainAccessProviders;
-	public CheckBox useKeychainCheckbox;
 	private final FxApplicationWindows appWindows;
+	public CheckBox useKeychainCheckbox;
 	public ChoiceBox<KeychainAccessProvider> keychainBackendChoiceBox;
 	public CheckBox startHiddenCheckbox;
 	public CheckBox autoCloseVaultsCheckbox;
