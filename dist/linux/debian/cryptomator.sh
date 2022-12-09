36ed4554b226961615245b94b810ecb64cf37435
Armin Schrenk <armin.schrenk@skymatic.de>
2022-12-09 15:12:04 +0100

    Fixes #2570
    Dabian build: Make libffi7 a runtime dependency and preload it.

--- dist/linux/debian/cryptomator.sh
+++ dist/linux/debian/cryptomator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # fix for https://github.com/cryptomator/cryptomator/issues/1370
-export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/jni/libjffi-1.2.so
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/jni/libjffi-1.2.so:/usr/lib/x86_64-linux-gnu/libffi.so.7.1.0
 
 /usr/lib/cryptomator/bin/cryptomator $@
