86ccb1a58f26666eb53c2f1ff7e4051328e916fb
Armin Schrenk <armin.schrenk@skymatic.de>
2023-01-26 12:05:34 +0100

    changing volume provider requires restart due to macFUSE/FUSE-T lib loaded in same JVM

--- src/main/java/org/cryptomator/common/mount/MountModule.java
+++ src/main/java/org/cryptomator/common/mount/MountModule.java
@@ -2,11 +2,11 @@ package org.cryptomator.common.mount;
 
 import dagger.Module;
 import dagger.Provides;
-import org.cryptomator.common.ObservableUtil;
 import org.cryptomator.common.settings.Settings;
 import org.cryptomator.integrations.mount.MountService;
 
 import javax.inject.Singleton;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import java.util.List;
 
@@ -19,6 +19,8 @@ public class MountModule {
 		return MountService.get().toList();
 	}
 
+	//currently not used, because macFUSE and FUSE-T cannot be used in the same JVM
+	/*
 	@Provides
 	@Singleton
 	static ObservableValue<ActualMountService> provideMountService(Settings settings, List<MountService> serviceImpls) {
@@ -29,6 +31,21 @@ public class MountModule {
 					return new ActualMountService(desiredService.orElse(fallbackProvider), desiredService.isPresent()); //
 				}, //
 				new ActualMountService(fallbackProvider, true));
+	}
+	 */
+
+	@Provides
+	@Singleton
+	static ActualMountService provideActualMountService(Settings settings, List<MountService> serviceImpls) {
+		var fallbackProvider = serviceImpls.stream().findFirst().orElse(null);
+		var desiredService = serviceImpls.stream().filter(serviceImpl -> serviceImpl.getClass().getName().equals(settings.mountService().getValue())).findFirst(); //
+		return new ActualMountService(desiredService.orElse(fallbackProvider), desiredService.isPresent()); //
+	}
+
+	@Provides
+	@Singleton
+	static ObservableValue<ActualMountService> provideMountService(ActualMountService service) {
+		return new SimpleObjectProperty<>(service);
 	}
 
 }
