385574a618a641e9ab2642e1bd0be27ab2fc96aa
Armin Schrenk <armin.schrenk@skymatic.de>
2023-03-15 12:10:15 +0100

    prevent infinite loop

--- src/main/java/org/cryptomator/common/mount/MountPointPreparationException.java
+++ src/main/java/org/cryptomator/common/mount/MountPointPreparationException.java
@@ -2,6 +2,10 @@ package org.cryptomator.common.mount;
 
 public class MountPointPreparationException extends RuntimeException {
 
+	public MountPointPreparationException(String msg) {
+		super(msg);
+	}
+
 	public MountPointPreparationException(Throwable cause) {
 		super(cause);
 	}
