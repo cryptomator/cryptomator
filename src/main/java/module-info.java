88e413681d0c95351d2b29499c3ebe771ac0a66e
Ralph Plawetzki <ralph@purejava.org>
2023-04-14 07:15:33 +0200

    Switch to "minimal" version of bindings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -32,7 +32,7 @@ open module org.cryptomator.desktop {
 	requires com.tobiasdiez.easybind;
 	requires dagger;
 	requires io.github.coffeelibs.tinyoauth2client;
-	requires libappindicator.gtk3.java;
+	requires libappindicator.gtk3.minimal.java;
 	requires org.slf4j;
 	requires org.apache.commons.lang3;
 
