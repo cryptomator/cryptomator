f91a854b3e157315a7d78828732f33df731cf66b
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2023-08-07 14:56:01 +0200

    removed more info hyperlink and fixed ui by moving isLoadingHttpResponse.set(true)

--- src/main/java/org/cryptomator/ui/error/ErrorController.java
+++ src/main/java/org/cryptomator/ui/error/ErrorController.java
@@ -43,7 +43,6 @@ public class ErrorController implements FxController {
 	private static final ObjectMapper JSON = new ObjectMapper();
 	private static final Logger LOG = LoggerFactory.getLogger(ErrorController.class);
 	private static final String ERROR_CODES_URL = "https://api.cryptomator.org/desktop/error-codes.json";
-	private static final String LOOKUP_PERMISSION_INFO_URL = "https://docs.cryptomator.org/"; //TODO: set correct url
 	private static final String SEARCH_URL_FORMAT = "https://github.com/cryptomator/cryptomator/discussions/categories/errors?discussions_q=category:Errors+%s";
 	private static final String REPORT_URL_FORMAT = "https://github.com/cryptomator/cryptomator/discussions/new?category=Errors&title=Error+%s&body=%s";
 	private static final String SEARCH_ERRORCODE_DELIM = " OR ";
@@ -136,17 +135,13 @@ public class ErrorController implements FxController {
 	}
 
 	@FXML
-	public void visitLookupPermissionInfoPage() {
-		application.getHostServices().showDocument(LOOKUP_PERMISSION_INFO_URL);
-	}
-
-	@FXML
 	public void dismiss() {
 		lookupDatabaseUserPermission.set(true);
 	}
 
 	@FXML
 	public void lookUpSolution() {
+		isLoadingHttpResponse.set(true);
 		lookupDatabaseUserPermission.set(true);
 		HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
 		HttpRequest httpRequest = HttpRequest.newBuilder()//
@@ -158,7 +153,6 @@ public class ErrorController implements FxController {
 	}
 
 	private void loadHttpResponse(HttpResponse<InputStream> response) {
-		isLoadingHttpResponse.set(true);
 		if (response.statusCode() != 200) {
 			LOG.error("Status code {} when trying to load {} ", response.statusCode(), response.uri());
 		}
