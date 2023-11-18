2a7a3a0f6d953723422e7cd3bc450a2781ede6c0
Parth Modi <pr571545@dal.ca>
2023-11-18 12:33:39 -0400

    used push down variable technique to remove the unnecessary abstraction code smell and also removed unused code from the class

--- src/main/java/org/cryptomator/ui/health/ResultListCellFactory.java
+++ src/main/java/org/cryptomator/ui/health/ResultListCellFactory.java
@@ -23,37 +23,31 @@ public class ResultListCellFactory implements Callback<ListView<Result>, ListCel
 		this.fxmlLoaders = fxmlLoaders;
 	}
 
+	// using push down variable technique to remove the unnecessary abstraction code smell
+	// removed the nested class as they are unnecessary and not required
 	@Override
 	public ListCell<Result> call(ListView<Result> param) {
 		try {
 			FXMLLoader fxmlLoader = fxmlLoaders.load("/fxml/health_result_listcell.fxml");
-			return new ResultListCellFactory.Cell(fxmlLoader.getRoot(), fxmlLoader.getController());
+			Parent node = fxmlLoader.getRoot();
+			ResultListCellController controller = fxmlLoader.getController();
+
+			return new ListCell<Result>() {
+				@Override
+				protected void updateItem(Result item, boolean empty) {
+					super.updateItem(item, empty);
+					if (item == null || empty) {
+						setText(null);
+						setGraphic(null);
+					} else {
+						controller.setResult(item);
+						setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+						setGraphic(node);
+					}
+				}
+			};
 		} catch (IOException e) {
 			throw new UncheckedIOException("Failed to load /fxml/health_result_listcell.fxml.", e);
-		}
-	}
-
-	private static class Cell extends ListCell<Result> {
-
-		private final Parent node;
-		private final ResultListCellController controller;
-
-		public Cell(Parent node, ResultListCellController controller) {
-			this.node = node;
-			this.controller = controller;
-		}
-
-		@Override
-		protected void updateItem(Result item, boolean empty) {
-			super.updateItem(item, empty);
-			if (item == null || empty) {
-				setText(null);
-				setGraphic(null);
-			} else {
-				controller.setResult(item);
-				setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-				setGraphic(node);
-			}
 		}
 	}
 }
