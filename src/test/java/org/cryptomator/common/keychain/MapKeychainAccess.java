780f3c0944ab1458670ad3f1c32a3f2d4fdfad89
Ralph Plawetzki <ralph@purejava.org>
2024-07-28 17:52:17 +0200

    Grab new API

--- src/test/java/org/cryptomator/common/keychain/MapKeychainAccess.java
+++ src/test/java/org/cryptomator/common/keychain/MapKeychainAccess.java
@@ -29,6 +29,11 @@ class MapKeychainAccess implements KeychainAccessProvider {
 	}
 
 	@Override
+	public void storePassphraseForAuthenticatedUser(String key, String displayName,CharSequence passphrase) {
+		storePassphrase(key, displayName, passphrase);
+	}
+
+	@Override
 	public char[] loadPassphrase(String key) {
 		return map.get(key);
 	}
