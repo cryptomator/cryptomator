12929369043bd88b1177e4d1c3f5ecd80b926441
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2024-09-13 11:49:05 +0200

    clean up

--- src/main/java/org/cryptomator/ui/removecert/RemoveCertController.java
+++ src/main/java/org/cryptomator/ui/removecert/RemoveCertController.java
@@ -2,8 +2,6 @@ package org.cryptomator.ui.removecert;
 
 import org.cryptomator.common.settings.Settings;
 import org.cryptomator.ui.common.FxController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javafx.fxml.FXML;
@@ -11,8 +9,6 @@ import javafx.stage.Stage;
 
 @RemoveCertScoped
 public class RemoveCertController implements FxController {
-
-	private static final Logger LOG = LoggerFactory.getLogger(org.cryptomator.ui.removecert.RemoveCertController.class);
 
 	private final Stage window;
 	private final Settings settings;
