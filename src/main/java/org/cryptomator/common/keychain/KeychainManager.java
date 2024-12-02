929782c3189986e2c01e7aade6de4930797643ca
Ralph Plawetzki <ralph@purejava.org>
2024-12-02 08:11:00 +0100

    Revert "Fix doubled method due to merge error"
    This reverts commit b5dbfd320913bc9d22a04367feffe011a0365779.

--- src/main/java/org/cryptomator/common/keychain/KeychainManager.java
+++ src/main/java/org/cryptomator/common/keychain/KeychainManager.java
@@ -44,6 +44,11 @@ public class KeychainManager implements KeychainAccessProvider {
 	}
 
 	@Override
+	public void storePassphrase(String key, String displayName, CharSequence passphrase, boolean ignored) throws KeychainAccessException {
+		getKeychainOrFail().storePassphrase(key, displayName, passphrase);
+		setPassphraseStored(key, true);
+	}
+
 	public void storePassphrase(String key, String displayName, CharSequence passphrase, boolean requireOsAuthentication) throws KeychainAccessException {
 		getKeychainOrFail().storePassphrase(key, displayName, passphrase, requireOsAuthentication);
 		setPassphraseStored(key, true);
