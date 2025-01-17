3b8fec4c5a0af5eb95e9a7c18df5993149dd3cf4
Armin Schrenk <armin.schrenk@skymatic.de>
2025-01-17 15:12:38 +0100

    Feature: Use system certificate stores/custom pkcs12 file (#3675)
    * for Windows use Windows certificate
    * for macOS use macOS Keychain
    * for Linux use a custom PKCS12 file under /etc/cryptomator/certs.p12

---
+++ src/main/java/org/cryptomator/networking/SSLContextWithMacKeychain.java
@@ -0,0 +1,21 @@
+package org.cryptomator.networking;
+
+import org.cryptomator.integrations.common.OperatingSystem;
+
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+/**
+ * SSLContextProvider for macOS using the macOS Keychain as truststore
+ */
+@OperatingSystem(OperatingSystem.Value.MAC)
+public class SSLContextWithMacKeychain extends SSLContextDifferentTrustStoreBase implements SSLContextProvider {
+
+	@Override
+	KeyStore getTruststore() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+		return KeyStore.getInstance("KeychainStore-ROOT");
+	}
+}
