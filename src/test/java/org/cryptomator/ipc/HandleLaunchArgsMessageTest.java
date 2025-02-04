09aa383258113b559d5ecb9f5407e88875940275
JyothiSwaroopReddy07 <bjyothiswaroop2002@gmail.com>
2025-02-04 01:03:17 -0800

    added handle launch arg message partition tests

--- src/test/java/org/cryptomator/ipc/HandleLaunchArgsMessageTest.java
+++ src/test/java/org/cryptomator/ipc/HandleLaunchArgsMessageTest.java
@@ -3,8 +3,9 @@ package org.cryptomator.ipc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-
+import java.nio.charset.StandardCharsets;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -13,10 +14,10 @@ import java.util.List;
 public class HandleLaunchArgsMessageTest {
 
 	@Test
-	public void testSendAndReceive(@TempDir Path tmpDir) throws IOException {
-		var message = new HandleLaunchArgsMessage(List.of("hello world", "foo bar"));
+	public void testSendAndReceiveSingleArgument(@TempDir Path tmpDir) throws IOException {
+		var message = new HandleLaunchArgsMessage(List.of("singleArgument"));
 
-		var file = tmpDir.resolve("tmp.file");
+		var file = tmpDir.resolve("single.tmp");
 		try (var ch = FileChannel.open(file, StandardOpenOption.CREATE_NEW, StandardOpenOption.READ, StandardOpenOption.WRITE)) {
 			message.send(ch);
 			ch.position(0);
@@ -29,10 +30,10 @@ public class HandleLaunchArgsMessageTest {
 	}
 
 	@Test
-	public void testSendAndReceiveEmpty(@TempDir Path tmpDir) throws IOException {
-		var message = new HandleLaunchArgsMessage(List.of());
+	public void testSendAndReceiveMultipleArguments(@TempDir Path tmpDir) throws IOException {
+		var message = new HandleLaunchArgsMessage(List.of("arg1", "arg2", "arg3"));
 
-		var file = tmpDir.resolve("tmp.file");
+		var file = tmpDir.resolve("multiple.tmp");
 		try (var ch = FileChannel.open(file, StandardOpenOption.CREATE_NEW, StandardOpenOption.READ, StandardOpenOption.WRITE)) {
 			message.send(ch);
 			ch.position(0);
@@ -42,5 +43,42 @@ public class HandleLaunchArgsMessageTest {
 				Assertions.fail("Received message of unexpected class");
 			}
 		}
+	}
+
+	@Test
+	public void testSendAndReceiveWithEmptyArguments(@TempDir Path tmpDir) throws IOException {
+		var message = new HandleLaunchArgsMessage(List.of());
+
+		var file = tmpDir.resolve("empty.tmp");
+		try (var ch = FileChannel.open(file, StandardOpenOption.CREATE_NEW, StandardOpenOption.READ, StandardOpenOption.WRITE)) {
+			message.send(ch);
+			ch.position(0);
+			if (IpcMessage.receive(ch) instanceof HandleLaunchArgsMessage received) {
+				Assertions.assertArrayEquals(message.args().toArray(), received.args().toArray());
+			} else {
+				Assertions.fail("Received message of unexpected class");
+			}
+		}
+	}
+
+	@Test
+	public void testDecodeSpecialCharacters() {
+		ByteBuffer buffer = ByteBuffer.wrap("arg1\narg2\narg3".getBytes(StandardCharsets.UTF_8));
+		HandleLaunchArgsMessage message = HandleLaunchArgsMessage.decode(buffer);
+		Assertions.assertEquals(List.of("arg1", "arg2", "arg3"), message.args());
+	}
+
+	@Test
+	public void testDecodeEmptyPayload() {
+		ByteBuffer buffer = ByteBuffer.wrap("".getBytes(StandardCharsets.UTF_8));
+		HandleLaunchArgsMessage message = HandleLaunchArgsMessage.decode(buffer);
+		Assertions.assertEquals(List.of(), message.args());
+	}
+
+	@Test
+	public void testDecodeOnlyDelimiters() {
+		ByteBuffer buffer = ByteBuffer.wrap("\n\n\n".getBytes(StandardCharsets.UTF_8));
+		HandleLaunchArgsMessage message = HandleLaunchArgsMessage.decode(buffer);
+		Assertions.assertEquals(List.of(), message.args());
 	}
 }
