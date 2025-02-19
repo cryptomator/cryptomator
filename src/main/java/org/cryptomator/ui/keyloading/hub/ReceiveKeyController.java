fc7169f2a03e67291b55d3c3f7d9386caf8da7d2
Julian Raufelder <Julian@Raufelder.com>
2025-02-19 09:05:13 +0100

    Send DeviceId to Hub during vault key retrieval
    See https://github.com/cryptomator/hub/pull/320

--- src/main/java/org/cryptomator/ui/keyloading/hub/ReceiveKeyController.java
+++ src/main/java/org/cryptomator/ui/keyloading/hub/ReceiveKeyController.java
@@ -165,6 +165,7 @@ public class ReceiveKeyController implements FxController {
 		var vaultKeyUri = hubConfig.URIs.API.resolve("vaults/" + vaultId + "/access-token");
 		var request = HttpRequest.newBuilder(vaultKeyUri) //
 				.header("Authorization", "Bearer " + bearerToken) //
+				.header("deviceId", deviceId) //
 				.GET() //
 				.timeout(REQ_TIMEOUT) //
 				.build();
