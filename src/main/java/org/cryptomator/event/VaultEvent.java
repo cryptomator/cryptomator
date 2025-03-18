83b557b6be216a617eab814d0b342e760df78706
Armin Schrenk <armin.schrenk@skymatic.de>
2025-03-18 18:02:55 +0100

    removed dead code

--- src/main/java/org/cryptomator/event/VaultEvent.java
+++ src/main/java/org/cryptomator/event/VaultEvent.java
@@ -3,28 +3,19 @@ package org.cryptomator.event;
 import org.cryptomator.common.vaults.Vault;
 import org.cryptomator.cryptofs.event.FilesystemEvent;
 
-import java.time.Instant;
-
 public record VaultEvent(Vault v, FilesystemEvent actualEvent, int count) implements Comparable<VaultEvent> {
-
-	public VaultEvent(Vault v, FilesystemEvent actualEvent) {
-		this(v, actualEvent, 1);
-	}
 
 	@Override
 	public int compareTo(VaultEvent other) {
 		var timeResult = actualEvent.getTimestamp().compareTo(other.actualEvent().getTimestamp());
-		if(timeResult != 0) {
+		if (timeResult != 0) {
 			return timeResult;
 		}
 		var vaultIdResult = v.getId().compareTo(other.v.getId());
-		if(vaultIdResult != 0) {
+		if (vaultIdResult != 0) {
 			return vaultIdResult;
 		}
 		return this.actualEvent.getClass().getName().compareTo(other.actualEvent.getClass().getName());
 	}
 
-	public VaultEvent incrementCount(FilesystemEvent update) {
-		return new VaultEvent(v, update, count+1);
-	}
 }
