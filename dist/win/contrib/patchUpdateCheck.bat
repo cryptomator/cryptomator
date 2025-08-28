a61a3cd3ac6af88e89387ae07b6d7933c2d8d20f
Armin Schrenk <armin.schrenk@skymatic.de>
2025-08-28 18:13:53 +0200

    strip trailing slash instead of whitespace workaround

--- dist/win/contrib/patchUpdateCheck.bat
+++ dist/win/contrib/patchUpdateCheck.bat
@@ -10,11 +10,13 @@ set "DISABLEUPDATECHECK=%~2"
 echo INSTALLDIR=%INSTALLDIR%
 echo DISABLEUPDATECHECK=%DISABLEUPDATECHECK%
 
+:: Normalize install directory (remove trailing slash if present)
+if "%INSTALLDIR:~-1%"=="\" set "INSTALLDIR=%INSTALLDIR:~0,-1%"
+
+
 :: Execute the PowerShell script
-:: DO NOT REMOVE WHITE SPACE AFTER %INSTALLDIR%.
-:: The variable is expanded to a value ending with a slash "\", which would escape the double quote breaking the parameter parsing
-powershell.exe -NoLogo -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned -File "%INSTALLDIR%patchUpdateCheck.ps1"^
- -InstallDir "%INSTALLDIR% "^
+powershell.exe -NoLogo -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned -File "%INSTALLDIR%\patchUpdateCheck.ps1"^
+ -InstallDir "%INSTALLDIR%"^
  -DisableUpdateCheck "%DISABLEUPDATECHECK%" 
 
 :: Return the exit code from PowerShell
