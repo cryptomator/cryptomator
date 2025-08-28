d370928e1c1582442c32452fea7eee3a41faa29c
Armin Schrenk <armin.schrenk@skymatic.de>
2025-08-28 17:59:35 +0200

    Ensure proper parameter injection for powershell script

--- dist/win/contrib/patchUpdateCheck.bat
+++ dist/win/contrib/patchUpdateCheck.bat
@@ -3,15 +3,19 @@
 :: This is executed as a Custom Action during MSI installation
 
 :: Get the install directory from the Custom Action property
-set "INSTALLDIR=%~1"
+set "INSTALLDIR=%~dp1"
 set "DISABLEUPDATECHECK=%~2"
 
 :: Log for debugging
 echo INSTALLDIR=%INSTALLDIR%
 echo DISABLEUPDATECHECK=%DISABLEUPDATECHECK%
 
-:: Execute the PowerShell script with bypass execution policy
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%INSTALLDIR%patchUpdateCheck.ps1" -InstallDir "%INSTALLDIR%" -DisableUpdateCheck "%DISABLEUPDATECHECK%"
+:: Execute the PowerShell script
+:: DO NOT REMOVE WHITE SPACE AFTER %INSTALLDIR%.
+:: The variable is expanded to a value ending with a slash "\", which would escape the double quote breaking the parameter parsing
+powershell.exe -NoLogo -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned -File "%INSTALLDIR%patchUpdateCheck.ps1"^
+ -InstallDir "%INSTALLDIR% "^
+ -DisableUpdateCheck "%DISABLEUPDATECHECK%" 
 
 :: Return the exit code from PowerShell
 exit /b %ERRORLEVEL%
