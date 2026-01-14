61be8c449c495c87511d17aac9a7d7549048dec3
Armin Schrenk <armin.schrenk@skymatic.de>
2026-01-14 17:39:49 +0100

    fixed flush does not clear BufferedLog

--- src/main/java/org/cryptomator/launcher/BufferedLog.java
+++ src/main/java/org/cryptomator/launcher/BufferedLog.java
@@ -9,20 +9,24 @@ class BufferedLog {
 
 	private final static List<Entry> logMessages = new ArrayList<>();
 
+	private BufferedLog() {
+	}
+
 	record Entry(String className, String message, List<Object> messageInput) {}
 
 	static void log(String className, String message, List<Object> messageInput) {
 		add(new BufferedLog.Entry(className, message, messageInput));
 	}
 
-	static void add(Entry e) {
+	synchronized static void add(Entry e) {
 		logMessages.add(e);
 	}
 
-	public static void flush(Logger log) {
+	synchronized static void flush(Logger log) {
 		logMessages.forEach(e -> {
 			var message = "PRE LOG INIT Event in %s: %s".formatted(e.className, e.message);
 			log.info(message, e.messageInput.toArray());
 		});
+		logMessages.clear();
 	}
 }
