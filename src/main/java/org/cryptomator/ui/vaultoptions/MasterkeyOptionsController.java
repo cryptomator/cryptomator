2a5bce2c5c340e57b3e32c9fe9ee80ce72bb8ddb
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2026-01-27 17:42:04 +0100

    disable password tab when masterkey.file is missing

--- src/main/java/org/cryptomator/ui/vaultoptions/MasterkeyOptionsController.java
+++ src/main/java/org/cryptomator/ui/vaultoptions/MasterkeyOptionsController.java
@@ -1,5 +1,6 @@
 package org.cryptomator.ui.vaultoptions;
 
+import dagger.Lazy;
 import org.cryptomator.common.keychain.KeychainManager;
 import org.cryptomator.common.recovery.RecoveryActionType;
 import org.cryptomator.common.vaults.Vault;
@@ -9,28 +10,38 @@ import org.cryptomator.ui.forgetpassword.ForgetPasswordComponent;
 import org.cryptomator.ui.recoverykey.RecoveryKeyComponent;
 
 import javax.inject.Inject;
+import javafx.application.Application;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
+import java.nio.file.Files;
+
+import static org.cryptomator.common.Constants.MASTERKEY_FILENAME;
 
 @VaultOptionsScoped
 public class MasterkeyOptionsController implements FxController {
 
+	private static final String DOCS_URL = "https://docs.cryptomator.org/desktop/"; //TODO: replace with correct docs link
+
 	private final Vault vault;
+	private final Lazy<Application> application;
 	private final Stage window;
 	private final ChangePasswordComponent.Builder changePasswordWindow;
 	private final RecoveryKeyComponent.Factory recoveryKeyWindow;
 	private final ForgetPasswordComponent.Builder forgetPasswordWindow;
 	private final KeychainManager keychain;
 	private final ObservableValue<Boolean> passwordSaved;
+	private final BooleanProperty masterkeyFileAvailable;
 
 
 	@Inject
-	MasterkeyOptionsController(@VaultOptionsWindow Vault vault, @VaultOptionsWindow Stage window, ChangePasswordComponent.Builder changePasswordWindow, RecoveryKeyComponent.Factory recoveryKeyWindow, ForgetPasswordComponent.Builder forgetPasswordWindow, KeychainManager keychain) {
+	MasterkeyOptionsController(@VaultOptionsWindow Vault vault, @VaultOptionsWindow Stage window, ChangePasswordComponent.Builder changePasswordWindow, RecoveryKeyComponent.Factory recoveryKeyWindow, ForgetPasswordComponent.Builder forgetPasswordWindow, KeychainManager keychain, Lazy<Application> application) {
 		this.vault = vault;
 		this.window = window;
+		this.application = application;
 		this.changePasswordWindow = changePasswordWindow;
 		this.recoveryKeyWindow = recoveryKeyWindow;
 		this.forgetPasswordWindow = forgetPasswordWindow;
@@ -40,6 +51,7 @@ public class MasterkeyOptionsController implements FxController {
 		} else {
 			this.passwordSaved = new SimpleBooleanProperty(false);
 		}
+		this.masterkeyFileAvailable = new SimpleBooleanProperty(Files.exists(vault.getPath().resolve(MASTERKEY_FILENAME)));
 	}
 
 	@FXML
@@ -63,11 +75,24 @@ public class MasterkeyOptionsController implements FxController {
 		forgetPasswordWindow.vault(vault).owner(window).build().showForgetPassword();
 	}
 
+	@FXML
+	public void openDocs() {
+		application.get().getHostServices().showDocument(DOCS_URL);
+	}
+
 	public ObservableValue<Boolean> passwordSavedProperty() {
 		return passwordSaved;
 	}
 
 	public boolean isPasswordSaved() {
 		return passwordSaved.getValue();
+	}
+
+	public BooleanProperty masterkeyFileAvailableProperty() {
+		return masterkeyFileAvailable;
+	}
+
+	public boolean isMasterkeyFileAvailable() {
+		return masterkeyFileAvailable.get();
 	}
 }
