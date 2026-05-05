74f36f20ea579305fb6d30941fa580f01f6deb9a
Armin Schrenk <armin.schrenk@skymatic.de>
2026-05-05 17:49:22 +0200

    remove legacy fuse2 stuff

--- dist/linux/flatpak/build-aux/fusermount-wrapper.sh
+++ dist/linux/flatpak/build-aux/fusermount-wrapper.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# From: https://gitlab.gnome.org/GNOME/gnome-builder/-/blob/main/build-aux/flatpak/fusermount-wrapper.sh
-
-if [ -z "$_FUSE_COMMFD" ]; then
-    FD_ARGS=
-else
-    FD_ARGS="--env=_FUSE_COMMFD=${_FUSE_COMMFD} --forward-fd=${_FUSE_COMMFD}"
-fi
-
-if [ -e /proc/self/fd/3 ] && [ 3 != "$_FUSE_COMMFD" ]; then
-    FD_ARGS="$FD_ARGS --forward-fd=3"
-fi
-
-exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount3 "$@"
+# based on https://gitlab.gnome.org/GNOME/gnome-builder/-/blob/main/build-aux/flatpak/fusermount-wrapper.sh
+# FUSE3 requires a socket for communication, its file descriptor id is given in the _FUSE_COMMFD variable
+exec flatpak-spawn --host --watch-bus --forward-fd=1 --forward-fd=2 --env=_FUSE_COMMFD=${_FUSE_COMMFD} --forward-fd=${_FUSE_COMMFD} fusermount3 "$@"
